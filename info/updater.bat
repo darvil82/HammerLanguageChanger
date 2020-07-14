d66778fddad7ccf032d81bb48608e3c403f39aa4
David Losantos (DarviL) <48654552+L89David@users.noreply.github.com>
2020-07-14 21:11:48 +0200

    Installer 1.1
    Added more info and now it wont download all the files at the start, it will download them only when requested.
    No more files to data folder, now everything goes to %temp%.

--- info/updater.bat
+++ info/updater.bat
@@ -3,22 +3,21 @@
 @echo off
 color f1
 chcp 65001 >Nul
-set "mode=mode con cols=55 lines=20"
+set "mode=mode con cols=55 lines=10"
 set ver=1.0.0
 title Hammer Language Changer Updater - V%ver%
 cls
 
+if not defined %1 exit
+
+echo msgbox ("Downloading the latest version. The installation will finish automatically. Please, wait...") > "%temp%/HLC/update_msg.vbs"
+
 
 %mode%
-echo:
-echo    ╔═══════════════════════════════════════════════╗
-echo    ║ Hammer Language Changer Updater         [▒▒▒] ║
-echo    ╟───────────────────────────────────────────────╢
-echo    ║ Downloading latest version. The installation  ║
-echo    ║ will finish automatically.                    ║
-echo    ║ Please, wait...                               ║
-echo    ╚═══════════════════════════════════════════════╝
+start "" "%temp%/HLC/update_msg.vbs"
 bitsadmin /transfer /download https://github.com/L89David/HammerLanguageChanger/blob/master/HLCInstaller.bat?raw=true "%1/HLCInstaller.bat" >nul
 if exist "%1/data" rd "%1/data" /s /q
+taskkill /im wscript.exe /f >nul
 start "" "HLCInstaller.bat"
+	
 exit
